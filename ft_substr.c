4c097919fd79c1b375c120fd42fe5ffddceba0db
Dominique DORDAIN <ddordain@e2r9p7.clusters.42paris.fr>
2021-11-28 16:27:01 +0100

    calloc parameters in the good order and with better portability

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ddordain <ddordain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 12:01:22 by ddordain          #+#    #+#             */
-/*   Updated: 2021/11/23 12:01:22 by ddordain         ###   ########.fr       */
+/*   Updated: 2021/11/28 16:25:58 by ddordain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_calloc(1, 1));
 	s = s + start;
 	len = ft_min_size(len, ft_strlen(s));
-	substr = ft_calloc(1, len + 1);
+	substr = ft_calloc(len + 1, sizeof(char));
 	if (substr == NULL)
 		return (NULL);
 	return ((char *)ft_memcpy(substr, s, len));
